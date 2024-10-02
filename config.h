a4bfa0550753a998cf19d6d5fb5ff5032517086f
Brayan M. Salazar <this.brayan@proton.me>
2024-10-02 19:19:14 -0400

    Changed terminal, color, etc.

--- config.h
+++ config.h
@@ -9,16 +9,17 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mononoki Nerd Font Mono:size=13.5" };
-static const char dmenufont[]       = "mononoki Nerd Font Mono:size=14";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=13.5" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+/* static const char col_cyan[]        = "#005577"; */
+static const char col_cyan[]        = "#633ec2";
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -38,8 +39,8 @@ static const char *const autostart[] = {
     "xset", "s", "off", NULL, /* Disable screensaver */
     "xset", "s", "noblank", NULL, /* Disable screen blank */
     "gentoo-pipewire-launcher", "restart", NULL,
-    /* "blueman-applet", NULL, */
-    /* "nm-applet", NULL, */
+    "blueman-applet", NULL,
+    "nm-applet", NULL,
     NULL /* terminate */
 };
 
@@ -94,7 +95,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *termcmd[] = { "st", NULL };
+static const char *termcmd[] = { "alacritty", NULL };
 static const char *dmenucmd[] = {
     "dmenu_run", "-m", dmenumon,
     "-fn", dmenufont, "-nb", col_gray1,
